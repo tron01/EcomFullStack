a08b75dd079822d3c157bec54a0762ff6bdb9ff9
tron01 <69385258+tron01@users.noreply.github.com>
2025-05-30 21:57:14 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -8,8 +8,5 @@ router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', logout);
 
-router.get('/admin', protect, authorizeRoles('admin'), (req, res) => {
-  res.json({ message: 'Welcome Admin' });
-});
 
 module.exports = router;
