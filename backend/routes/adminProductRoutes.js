391791db643de237eb4c6a0ca5077ce376c7f5f2
tron01 <69385258+tron01@users.noreply.github.com>
2025-06-01 01:56:18 +0530

    Create adminProductRoutes.js

---
+++ backend/routes/adminProductRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { 
+  createProduct, 
+  updateProduct, 
+  deleteProduct, 
+  getAllProducts, 
+  getProductById,
+  addOrUpdateProductImages,
+} = require('../controllers/productController');
+const { protect, authorizeRoles } = require('../middlewares/authMiddleware');
+const upload = require('../middlewares/upload');
+
+const router = express.Router();
+
+router.post('/', protect, authorizeRoles('admin'), createProduct);
+router.put('/:id', protect, authorizeRoles('admin'), updateProduct);
+
+router.post('/:id/images',protect,authorizeRoles('admin'),
+  upload.fields([
+    { name: 'thumbnail', maxCount: 1 },
+    { name: 'images', maxCount: 3 }
+  ]),
+  addOrUpdateProductImages);
+
+router.get('/', protect, authorizeRoles('admin'), getAllProducts);
+router.get('/:id', protect, authorizeRoles('admin'), getProductById);
+router.delete('/:id', protect, authorizeRoles('admin'), deleteProduct);
+
+module.exports = router;
