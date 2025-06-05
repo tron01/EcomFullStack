fab96a7347265f31ce8aa24491bcbee20e5e13a0
tron01 <69385258+tron01@users.noreply.github.com>
2025-06-05 21:43:04 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,14 +3,55 @@ const router = express.Router();
 const { getProfile } = require('../controllers/userController');
 const { protect } = require('../middlewares/authMiddleware');
 const { getAllActivePaymentMethods } = require('../controllers/paymentMethodController');
+const {  login, logout  } = require('../controllers/authController');
 
 
 
 
 router.get('/payment-methods',protect, getAllActivePaymentMethods);
 router.get('/profile', protect, getProfile);
+router.post('/login', login);
+router.post('/logout', protect, logout);
 
 module.exports = router;
+/**
+ * @swagger
+ * /user/login:
+ *   post:
+ *     summary: Login a user
+ *     tags: [User]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *       401:
+ *         description: Invalid credentials
+ */
+
+/**
+ * @swagger
+ * /user/logout:
+ *   post:
+ *     summary: Logout the current user
+ *     tags: [User]
+ *     security:
+ *       - cookieAuth: []
+ *     responses:
+ *       200:
+ *         description: Logout successful
+ *       401:
+ *         description: Unauthorized
+ */
 
 /**
  * @swagger
